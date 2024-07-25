d3dfda562cf62057643f21586386b88d2778a2e9
Tata <marnovaanastasia.default@gmail.com>
2024-07-25 11:27:08 +0300

    srep1

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greetingUser = () => {
     const playerName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${playerName }!`);
+    console.log(`Hello, ${playerName}!`);
 }
 
 export default greetingUser;
