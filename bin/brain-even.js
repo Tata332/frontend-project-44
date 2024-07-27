4bfdee95b41017d4573103010f667943eca64378
Tata <marnovaanastasia.default@gmail.com>
2024-07-28 02:17:46 +0300

    step2

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,3 @@
-import  getNotfgot from '../games/even.js';
+import  getNotfgot from '../src/games/even.js';
 
 getNotfgot();
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,13 +1,13 @@
-import getEngine from '../src/index.js';
-import { getRandomNumber, getRandomOperator } from '../src/utils.js';
+import getEngine from '../index.js';
+import { getRandomNumber, getRandomOperator } from '../utils.js';
 
 const rule = 'What is the result of the expression?';
 
 const getQuestionAndAnswer = () => {
-    const equasion = randomEquasion(getRandomNumber(0, 100), getRandomNumber(0, 100));
+    const equasion = getRandomOperator(getRandomNumber(0, 100), getRandomNumber(0, 100));
     return [equasion.equasion, equasion.result];
 };
 
 export default () => {
     getEngine(rule, getQuestionAndAnswer);
-};;
+};
