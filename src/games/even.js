b099131aced4d11a68a5a36b36dab56dc9ed15c2
Tata <marnovaanastasia.default@gmail.com>
2024-07-27 23:51:40 +0300

    st

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,15 @@
-import getIndex from '../index.js';
+import gameEngine from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (num) => num % 2 === 0;
-const answer = () => {
+const getQuestionAndAnswer = () => {
   const question = getRandomNumber(0, 100);
-  const correctAnswer = isEven(question) ? "yes" : "no";
-  return [question, correctAnswer];
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
 };
 
 export default () => {
-  getIndex(rule, answer);
+  gameEngine(rule, getQuestionAndAnswer);
 };
