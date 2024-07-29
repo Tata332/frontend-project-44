7fe617dbc98f0ca6350752bf038813227fb0d967
Tata <marnovaanastasia.default@gmail.com>
2024-07-29 12:55:28 +0300

    examination utils

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,27 @@
 const getRandomNumber = (min, max) => {
-    const number = Math.floor(Math.random() * (max - min + 1)) + min;
-    return number;
-  };
+  const number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return number;
+};
   
-  const getRandomOperator = (num1, num2) => {
-    const operators = ['+', '-', '*'];
-    const randomSymbol = operators[Math.floor(Math.random() * operators.length)];
-    if (randomSymbol === '+') {
-      return {
-        equasion: `${num1} + ${num2}`,
-        result: `${num1 + num2}`,
-      };
-    } if (randomSymbol === '-') {
-      return {
-        equasion: `${num1} - ${num2}`,
-        result: `${num1 - num2}`,
-      };
-    }
+const getRandomOperator = (num1, num2) => {
+  const operators = ['+', '-', '*'];
+  const randomSymbol = operators[Math.floor(Math.random() * operators.length)];
+  if (randomSymbol === '+') {
     return {
-      equasion: `${num1} * ${num2}`,
-      result: `${num1 * num2}`,
+      equasion: `${num1} + ${num2}`,
+      result: `${num1 + num2}`,
     };
+  }
+  if (randomSymbol === '-') {
+    return {
+      equasion: `${num1} - ${num2}`,
+      result: `${num1 - num2}`,
+    };
+  }
+  return {
+    equasion: `${num1} * ${num2}`,
+    result: `${num1 * num2}`,
   };
+};
   
-  export { getRandomNumber, getRandomOperator };
+export { getRandomNumber, getRandomOperator };
