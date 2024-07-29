0914ac7c055018d19669979aa05dcdbb2ebd24ff
Tata <marnovaanastasia.default@gmail.com>
2024-07-29 12:38:07 +0300

    examination gcd

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,24 +4,23 @@ import { getRandomNumber} from '../utils.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const isPrime = (num) => {
-    if (num <= 1) {
+  if (num <= 1) {
+    return false;
+  }
+  let i = 2;
+  while (i <= (num / 2)) {
+    if (num % i === 0) {
       return false;
     }
-    let i = 2;
-    while (i <= (num / 2)) {
-        if (num % i === 0) {
-            return false;
-        }
-        i += 1;
-    }
+    i += 1;
+  }
     return true;
 };
 
 const getQuestionAndAnswer = () => {
-    const question = getRandomNumber(0, 100);
-    const answer = isPrime(question) ? 'yes' : 'no';
-    return [question, answer];
-  };
-
+  const question = getRandomNumber(0, 100);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return [question, answer];
+};
 
 export default () => getEngine(rule, getQuestionAndAnswer);
