2d26e62af91b598753a407008b9129454eed84f2
Tata <marnovaanastasia.default@gmail.com>
2024-08-02 22:23:09 +0300

    correction calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,12 +1,33 @@
 import getEngine from '../index.js';
-import { getRandomNumber, getRandomOperator } from '../utils.js';
+import { getRandomNumber } from '../utils.js';
 
 const rule = 'What is the result of the expression?';
 
-const getQuestionAndAnswer = () => {
-  const equasion = getRandomOperator(getRandomNumber(0, 100), getRandomNumber(0, 100));
-  return [equasion.equasion, equasion.result];
+const calculate = (num1, num2) => {
+  const operators = ['+', '-', '*'];
+  const randomSymbol = operators[Math.floor(Math.random() * operators.length)];
+  if (randomSymbol === '+') {
+    return {
+      equasion: `${num1} + ${num2}`,
+      result: `${num1 + num2}`,
+    };
+  }
+  if (randomSymbol === '-') {
+    return {
+      equasion: `${num1} - ${num2}`,
+      result: `${num1 - num2}`,
+    };
+  }
+  return {
+    equasion: `${num1} * ${num2}`,
+    result: `${num1 * num2}`,
+  };
 };
+
+const getQuestionAndAnswer = () => {
+  const equasion = calculate(getRandomNumber(0, 100), getRandomNumber(0, 100));
+  return [equasion.equasion, equasion.result];
+}
 
 export default () => {
   getEngine(rule, getQuestionAndAnswer);
--- src/utils.js
+++ src/utils.js
@@ -3,25 +3,4 @@ const getRandomNumber = (min, max) => {
   return number;
 };
 
-const getRandomOperator = (num1, num2) => {
-  const operators = ['+', '-', '*'];
-  const randomSymbol = operators[Math.floor(Math.random() * operators.length)];
-  if (randomSymbol === '+') {
-    return {
-      equasion: `${num1} + ${num2}`,
-      result: `${num1 + num2}`,
-    };
-  }
-  if (randomSymbol === '-') {
-    return {
-      equasion: `${num1} - ${num2}`,
-      result: `${num1 - num2}`,
-    };
-  }
-  return {
-    equasion: `${num1} * ${num2}`,
-    result: `${num1 * num2}`,
-  };
-};
-
-export { getRandomNumber, getRandomOperator };
+export { getRandomNumber };
