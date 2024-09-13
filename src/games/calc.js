5d9ccf7a23a9038b3c5d728373487927dd441d04
Tata <marnovaanastasia.default@gmail.com>
2024-09-13 13:58:23 +0300

    f

--- src/games/calc.js
+++ src/games/calc.js
@@ -14,7 +14,7 @@ const calculate = (number1, number2, operator) => {
     case '*':
       return number1 * number2;
     default:
-      throw new Error('Error');
+      throw new Error('Error ');
   }
 };
 
